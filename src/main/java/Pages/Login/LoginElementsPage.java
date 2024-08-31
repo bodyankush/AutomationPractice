c78d42610846b52ae75ec4ee4df97eff04aadceb
Bohdan Holovko QA Automation <holovkobusiness@gmail.com>
2024-08-31 15:32:05 +0300

    Removed LoginElements from the Login Test

--- src/main/java/Pages/Login/LoginElementsPage.java
+++ src/main/java/Pages/Login/LoginElementsPage.java
@@ -4,10 +4,9 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class LoginElementsPage {
-    public SelenideElement emailField = $x("//*[@id=\"i0116\"]");
-    public SelenideElement nextButton = $x("//*[@id=\"idSIButton9\"]");
-    public SelenideElement passwordField = $x("//*[@id=\"i0118\"]");
-    public SelenideElement signInButton = $x("//*[@id=\"idSIButton9\"]");
-    public SelenideElement staySignInNoButton = $x("//*[@id=\"idBtn_Back\"]");
-
+    public static SelenideElement emailField = $x("//*[@id=\"i0116\"]");
+    public static SelenideElement nextButton = $x("//*[@id=\"idSIButton9\"]");
+    public static SelenideElement passwordField = $x("//*[@id=\"i0118\"]");
+    public static SelenideElement signInButton = $x("//*[@id=\"idSIButton9\"]");
+    public static SelenideElement staySignInNoButton = $x("//*[@id=\"idBtn_Back\"]");
 }
--- src/test/java/Web/TestCases/Login/NeomUserCanBeSuccessfullyLoggedIn.java
+++ src/test/java/Web/TestCases/Login/NeomUserCanBeSuccessfullyLoggedIn.java
@@ -1,22 +1,16 @@
 package Web.TestCases.Login;
-
 import org.testng.annotations.Test;
 import Pages.Login.LoginElementsPage;
 import static com.codeborne.selenide.Selenide.open;
 
 public class NeomUserCanBeSuccessfullyLoggedIn {
-
     @Test
     public void loginFlow(){
         open("https://mysecuritytest.neom.com/");
-
-        LoginElementsPage loginElements = new LoginElementsPage();
-
-       loginElements.emailField.setValue("Quality.Automation@neom.com");
-       loginElements.nextButton.click();
-       loginElements.passwordField.setValue("Welcome@2024");
-       loginElements.signInButton.click();
-       loginElements.staySignInNoButton.click();
-
+        LoginElementsPage.emailField.setValue("Quality.Automation@neom.com");
+        LoginElementsPage.nextButton.click();
+        LoginElementsPage.passwordField.setValue("Welcome@2024");
+        LoginElementsPage.signInButton.click();
+        LoginElementsPage.staySignInNoButton.click();
     }
 }
